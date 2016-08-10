3e0de57e21f5dd6cca1f2a13ff4d6ac17a9d1584
jitendrathakur <jitendra.thakur2008@gmail.com>
2016-08-10 19:54:52 +0530

    print value from controller

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # welcome-es6
 getting started with es6 and babel
+
+# To run it with localhost
+## npm install http-server -g
+## http-server
---
+++ main.module.js
@@ -0,0 +1,8 @@
+var pioneer = angular.module('pioneer',  []);
+ // .config(routerConfig);
+
+
+pioneer.controller('indexCtrl', function($scope){
+
+    $scope.words = ['It','is','what','it','is']
+});
