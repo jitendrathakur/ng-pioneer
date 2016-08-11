e661ce4d2f2b5f19ff74d53bf1463407228296b8
jitendrathakur <jitendra.thakur2008@gmail.com>
2016-08-11 13:11:33 +0530

    two controller on same dom

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ getting started with es6 and babel
 # To run it with localhost
 ## npm install http-server -g
 ## http-server
+
+
+
+
+
+# b8f8019] print value from controller
--- main.module.js
+++ main.module.js
@@ -2,7 +2,12 @@ var pioneer = angular.module('pioneer',  []);
  // .config(routerConfig);
 
 
-pioneer.controller('indexCtrl', function($scope){
+pioneer.controller('firstCtrl', function($scope){
 
     $scope.words = ['It','is','what','it','is']
+});
+
+pioneer.controller('secondCtrl', function($scope){
+
+    $scope.animals = ['viper','cat','dog','jackal','owl']
 });
